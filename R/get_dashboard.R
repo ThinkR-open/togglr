1a1a11e3fe89aedcd5dc9409576d01fda6ea2f81
vincent <vincent@thinkr.fr>
2017-08-02 23:11:52 +0200

    get dash

---
+++ R/get_dashboard.R
@@ -0,0 +1,148 @@
+#' recupere les données depuis toggl.com
+#'
+#' @param api_token the toggl api token
+#' @param workspace_id the workspace id
+#' @param since begin date
+#' @param until stop date
+#'
+#' @export
+#'
+#' @importFrom lubridate years
+#' @importFrom  dplyr select
+#' @importFrom stats setNames
+#' 
+get_dashboard <- function(api_token=get_toggl_api_token(),
+                          workspace_id =get_workspace_id(),
+                          since = Sys.Date()-lubridate::years(1),
+                          until = Sys.Date()
+                          
+){
+  
+  url <- sprintf("https://toggl.com/reports/api/v2/summary?workspace_id=%s&since=%s&until=%s&user_agent=api_test",
+                 get_workspace_id(),
+                 format(since,"%Y-%m-%d"),
+                 format(until,"%Y-%m-%d")
+                 
+  )
+  wp <- content(GET(url,
+                    # verbose(),
+                    authenticate(api_token,"api_token"),
+                    encode="json"))
+  
+  
+  jsonlite:::simplify(wp$data, simplifyDataFrame = TRUE) -> out
+  
+  
+  synthese <-    data.frame(id = out$id, out$title, time = out$time) %>%
+    select(-color, -hex_color)
+  
+  
+  tache <- out$items %>% 
+    setNames(synthese$project) %>% 
+    map(~{.x$title <-  .x$title$time_entry;.x})
+  
+  res <- list(synthese = synthese,
+              tache = tache)
+  class(res) <- "toggl"
+  res
+}
+
+n_to_tps <- function(n){
+  format( as.POSIXct(Sys.Date())+n/1000
+          , "%M:%S")
+}
+
+
+
+n_to_tps <- function(n){
+  
+  format( 
+    as.POSIXct(0,origin=Sys.Date(), tz="GMT")+n/1000
+    , "%H:%M:%S")
+}
+#' get project total time
+#'
+#' @param project_name project name
+#' @param api_token the toggl api token
+#' @param workspace_id the workspace id
+#' @param since a date
+#' @param until a date
+#'
+#' @export
+#'
+get_project_total <- function(project_name = get_context_project(),
+                  api_token=get_toggl_api_token(),
+                      workspace_id =get_workspace_id(),
+                      since = Sys.Date()-lubridate::years(1),
+                      until = Sys.Date()){
+  
+  
+ dash <-  get_dashboard(api_token=api_token,workspace_id=workspace_id,since=since,until=until)
+dash$synthese  %>% filter(project == project_name) %>% pull(time) %>% n_to_tps
+}
+
+#' Get all project's names
+#'
+#' @param api_token the toggl api token
+#' @param workspace_id the workspace id
+#' @param since a date
+#' @param until a date
+#'
+#' @export
+#'
+get_all_project_names <- function(  api_token=get_toggl_api_token(),
+                                    workspace_id =get_workspace_id(),
+                                    since = Sys.Date()-lubridate::years(1),
+                                    until = Sys.Date()){
+  
+  
+  dash <-  get_dashboard(api_token=api_token,workspace_id=workspace_id,since=since,until=until)
+  dash$synthese$project
+}
+
+
+#' get all client's name
+#'
+#' @param api_token the toggl api token
+#' @param workspace_id the workspace id
+#' @param since a date
+#' @param until a date
+#'
+#' @return
+#' @export
+#'
+get_all_client_names <- function(  api_token=get_toggl_api_token(),
+                                    workspace_id =get_workspace_id(),
+                                    since = Sys.Date()-lubridate::years(1),
+                                    until = Sys.Date()){
+  
+  
+  dash <-  get_dashboard(api_token=api_token,workspace_id=workspace_id,since=since,until=until)
+  dash$synthese$client %>% unique()
+}
+
+#' get all task from a project
+#'
+#' @param project_name project name
+#' @param api_token the toggl api token
+#' @param workspace_id the workspace id
+#' @param since a date
+#' @param until a date
+#'
+#' @return
+#' @export
+#'
+get_project_task_detail <- function(
+  project_name = get_context_project(),
+  api_token=get_toggl_api_token(),
+  workspace_id =get_workspace_id(),
+  since = Sys.Date()-lubridate::years(1),
+  until = Sys.Date()
+  
+){
+  dash <-  get_dashboard(api_token=api_token,workspace_id=workspace_id,since=since,until=until)
+  dash$tache[[project_name]] %>% mutate(time=n_to_tps(time))
+}
+
+
+
