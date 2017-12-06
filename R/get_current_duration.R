ab4913014f7d1107cd3cc047bed2293e4e32c5d6
vincent <vincent@thinkr.fr>
2017-12-06 16:50:58 +0100

    duration improvment

--- R/get_current_duration.R
+++ R/get_current_duration.R
@@ -12,9 +12,14 @@
 get_current_duration <-
   function(api_token = get_toggl_api_token()) {
     # now() -  (get_current(api_token = api_token)$at %>% ymd_hms())
-    out <- difftime(now() ,  (get_current(api_token = api_token)$at %>% ymd_hms()),units = "secs")
+    out <- difftime(now() ,  (get_current(api_token = api_token)$at %>% ymd_hms()),units = "secs")*1000
     class(out) <- "toggl_time"
-  out
+    if (length(out) == 0){out <- 0}
+    out
     }
 
-print.toggl_time <- print.difftime
+print.toggl_time <- function(x){
+  print(prettyunits::pretty_ms(as.numeric(x)))
+}
+
+
