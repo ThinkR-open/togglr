aabf32149d8034beb74638433577672f7a087b42
vincent <vincent@thinkr.fr>
2018-01-04 16:53:12 +0100

    #' \dontrun{ #' get_time_entries() #' }

--- R/Get_time_entries.R
+++ R/Get_time_entries.R
@@ -13,7 +13,9 @@
 #' @importFrom parsedate format_iso_8601 parse_iso_8601
 #' @encoding UTF-8
 #' @examples
+#' \dontrun{
 #' get_time_entries()
+#' }
 get_time_entries <- function(api_token = get_toggl_api_token(),
                              since = Sys.time() - lubridate::weeks(1),
                              until = Sys.time()){
