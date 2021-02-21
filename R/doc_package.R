3d59c122e7efe14659a2d7f5e0d6ba6120b0c750
vincent <vincent@thinkr.fr>
2021-02-21 23:26:18 +0100

    check OK

--- R/doc_package.R
+++ R/doc_package.R
@@ -1,7 +1,7 @@
 #' This package allow user to track time  spend on R using the toggl.com timesheet
 #' 
 #' 
-#' 
+#' @description toggl.com API 
 #' @name togglr-package
 #' @aliases togglr-package togglr
 #' @docType package
